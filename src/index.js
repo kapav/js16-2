326b73cc139ec4acd933b4be4c8ce486de3009e5
Александр Кочкин <Alexander.Kochkin@artezio.com>
2019-05-19 23:18:17 +0400

    Добавлены css-loader, style-loader, подключение css, агрегирующий файл для css.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import AppService from './modules/app.service';
 import {config} from './modules/config';
 import './modules/header.component';
+import './css/index.css';
 
 console.log('Здравствуй, Вселенная!');
+
+console.log(css.toString());
 
 console.log('Config key:', config.key);
 
