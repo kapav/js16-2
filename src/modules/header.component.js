58396302a8df9263baaca3f10cfbf01905382e9b
Александр Кочкин <Alexander.Kochkin@artezio.com>
2019-05-19 22:36:16 +0400

    Добавление jquery; режима наблюдения за изменениями (watch-режима).

--- src/modules/header.component.js
+++ src/modules/header.component.js
@@ -1,1 +1,11 @@
+import $ from 'jquery';
+
+$('<h1 />')
+	.text('Здравствуй, Вселенная, - из jQuery!')
+	.css({
+		textAlign: 'center',
+		color: 'blue'
+	})
+	.appendTo($('header'));
+
 console.log('Header component.');
