120cbc5e592848c49c718a3ff5a535e047e06891
Александр Кочкин <Alexander.Kochkin@artezio.com>
2019-05-19 20:06:53 +0400

    Добавлен html-webpack-plugin.

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
 const path = require('path');
+const HtmlPlugin = require('html-webpack-plugin');
 
 module.exports = {
 	entry: './src/index.js',
 	output: {
 		filename: 'bundle.js',
 		path: path.resolve(__dirname, 'dist')
-	}
+	},
+	plugins: [
+		new HtmlPlugin({
+			filename: 'index.html',
+			template: './src/index.html'
+		})
+	]
 }
